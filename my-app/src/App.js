8caf5d12314dcc30567697a7551e572849e4dd3a
Cyberdev <turnerjeniah8@gmail.com>
2023-03-09 13:18:50 -0800

    Restored deleted index.css file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+import About from "./components/pages/Aboutme";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
