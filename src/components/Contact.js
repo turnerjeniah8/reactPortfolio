867c65a8bcde2c3cf19c71638f70111d278a4d27
Cyberdev <turnerjeniah8@gmail.com>
2023-03-09 03:34:56 -0800

    Finished contact.js file. Included all forms and ways to contact me.

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ContactPNG from "../../Assets/contact.jpg"
+
+
+function Contact(props) {
+    return (
+
+        <div className="contact-card">
+
+            <div className="card">
+                <h1 className="text-center">Contact Me</h1>
+                <div className="card-content">
+                    <a href="mailto:turnerjeniah1120@gmail.com" className="ml-4 mt-4 font-weight-bold">Email</a>
+
+                    <a href="" className="ml-4 mt-4 font-weight-bold">LinkedIn</a>
+
+                    <a href="https://www.linkedin.com/in/jeniah-t-b98470230" className="ml-4 mt-4 font-weight-bold">GitHub</a>
+
+                    <img src={ContactPNG} alt="profile-photo" className="profile float-left"></img>
+                </div>
+            </div>
+        </div >
+
+
+    );
+}
+
+export default Contact;
