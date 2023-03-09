2701cc3dc92dc5ec3f52f2082dab34528b9705f3
Cyberdev <turnerjeniah8@gmail.com>
2023-03-09 03:21:50 -0800

    Completed AboutmeCard.js Included aboutme paragraph and ways to contact me.

---
+++ src/components/Aboutme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutmeCard from '../AboutmeCard'
+
+function Aboutme() {
+  return (
+    <div>
+      <AboutmeCard />
+    </div>
+  );
+}
+
+export default Aboutme;
